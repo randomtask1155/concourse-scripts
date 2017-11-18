d22510bf032f1ffd2e1ba921801b65aa490f40f8
dlynch <dlynch@gopivotal.com>
2017-11-17 22:44:54 -0600

    fix her server reference

--- scripts/sso/deploy-sso-hero.sh
+++ scripts/sso/deploy-sso-hero.sh
@@ -10,7 +10,7 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 mv cf /usr/local/bin
 cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE --skip-ssl-validation
 
-sed -i s/https:\\/\\/hero.apps.domain/${SSO_SERVICE_INSTANCE}/g sso-hero-app/manifest.yml
+sed -i s/https:\\/\\/hero.apps.domain/https://hero.${CF_APP_DOMAIN}/g sso-hero-app/manifest.yml
 sed -i s/sso-danl/${SSO_SERVICE_INSTANCE}/g sso-hero-app/manifest.yml
 cd sso-hero-app
 cf push
