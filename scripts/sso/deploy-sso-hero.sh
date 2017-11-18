bd9e53af02fe01be19cddd091ee5b063d63160ad
dlynch <dlynch@gopivotal.com>
2017-11-17 21:18:00 -0600

    fix sso deployment

--- scripts/sso/deploy-sso-hero.sh
+++ scripts/sso/deploy-sso-hero.sh
@@ -2,12 +2,15 @@
 
 set -e 
 
+apt-get -y update
+apt-get -y install curl
+
 # install cf 
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE --skip-ssl-validation
 
 
-sed -i s/danl-sso/${SSO_SERVICE_INSTANCE}/g sso-hero-app/manifest.yml
+sed -i s/sso-danl/${SSO_SERVICE_INSTANCE}/g sso-hero-app/manifest.yml
 cd sso-hero-app
 cf push
